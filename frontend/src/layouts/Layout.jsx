01eb31d735f866dc8154f7e69821aae2d18c4183
jomi087 <jomijoseph087@gmail.com>
2026-06-30 00:36:40 +0530

    feat: add navbar UI and routing layout

---
+++ frontend/src/layouts/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+const Layout = () => {
+  return (
+    <div className="mx-auto max-w-7xl px-4">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
