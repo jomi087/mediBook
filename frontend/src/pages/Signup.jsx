63217afff9861f913905ce1313aa5efd8c64f12f
jomi087 <jomijoseph087@gmail.com>
2026-07-07 16:45:30 +0530

    feat(auth signup): implement signup from

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import AuthForm from '../components/AuthForm';
+
+const Signup = () => {
+  return (
+    <div className="min-h-[80vh] flex items-center justify-center px-4 py-5 pb-15">
+      <AuthForm mode="signup" />
+    </div>
+  );
+};
+
+export default Signup;
