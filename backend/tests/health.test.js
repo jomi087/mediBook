8c244f48cf8f9e5ff5b5d724175aaa8ff14900c4
jomi087 <jomijoseph087@gmail.com>
2026-07-14 21:41:08 +0530

    feat: add backend CI workflow, Dockerfile, and health check test

---
+++ backend/tests/health.test.js
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import { app } from '../app.js';
+
+describe('GET /health', () => {
+  test('should return a healthy status response', async () => {
+    //One specific test case
+
+    // Arrange & Act
+    const response = await request(app).get('/health');
+
+    //Assert
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      success: true,
+      message: 'Server is running',
+      data: null,
+    });
+  });
+});
